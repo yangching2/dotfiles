35914b6f3723efd465aa5a0622c5c98c02173fb8
Ching Yang <yangching1987@gmail.com>
2024-06-06 20:14:46 +0800

    enable copilot

--- nvim/lua/josean/lazy.lua
+++ nvim/lua/josean/lazy.lua
@@ -29,7 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
 
     { import = "josean.plugins" },
     { import = "josean.plugins.lsp" },
--- nvim/lua/josean/plugins/telescope.lua
+++ nvim/lua/josean/plugins/telescope.lua
@@ -30,7 +30,8 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            -- ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-t>"] = require("trouble.sources.telescope").open,
           },
         },
       },
